8b68dcb837b1b02c63200af85013e229126afef5
0xarchit <0xarchit@gmail.com>
2026-06-26 17:36:29 +0530

    Bind Bubble Tea's input/output to OS standard streams to fix black screen in release builds

--- tui/run.go
+++ tui/run.go
@@ -1,9 +1,19 @@
 package tui
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 func StartTUI() error {
-	p := tea.NewProgram(NewModel(), tea.WithAltScreen(), tea.WithMouseCellMotion())
+	p := tea.NewProgram(
+		NewModel(),
+		tea.WithAltScreen(),
+		tea.WithMouseCellMotion(),
+		tea.WithInput(os.Stdin),
+		tea.WithOutput(os.Stdout),
+	)
 	_, err := p.Run()
 	return err
 }
