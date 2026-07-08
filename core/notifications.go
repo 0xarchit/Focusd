3ff69363ee43c544865fc0eb2de7222b321e3dca
0xarchit <0xarchit@gmail.com>
2026-07-09 00:32:23 +0530

    feat: native Windows toast notifications via go-toast
    Replace PowerShell-based notifications with go-toast package for native
    Windows toast notifications. Notifications now show as 'Focusd' instead
    of 'Windows PowerShell'. Removes PowerShell spawning overhead.

--- core/notifications.go
+++ core/notifications.go
@@ -1,74 +1,47 @@
 package core
 
 import (
-	"sync/atomic"
-	"syscall"
 	"time"
-	"unsafe"
+
+	"github.com/go-toast/toast"
 )
 
-var (
-	lastNotificationTime int64
-	activeInstances      int32
-)
+var lastNotification time.Time
 
-var (
-	user32          = syscall.NewLazyDLL("user32.dll")
-	procMessageBoxW = user32.NewProc("MessageBoxW")
-)
-
-const (
-	mbOKCancel        = 0x00000001
-	mbIconInformation = 0x00000040
-	mbIconWarning     = 0x00000030
-	mbSetForeground   = 0x00010000
-	idOK              = 1
-)
-
-func show(title, message string, flags uintptr, callback func(ret uintptr)) bool {
-	now := time.Now().Unix()
-	last := atomic.LoadInt64(&lastNotificationTime)
-	if now-last < 10 {
+func showNotification(title, message string) bool {
+	if time.Since(lastNotification) < 10*time.Second {
 		return false
 	}
-	if !atomic.CompareAndSwapInt32(&activeInstances, 0, 1) {
-		return false
-	}
-	atomic.StoreInt64(&lastNotificationTime, now)
+	lastNotification = time.Now()
 
 	go func() {
-		defer atomic.StoreInt32(&activeInstances, 0)
-
-		titlePtr, err := syscall.UTF16PtrFromString(title)
-		if err != nil {
-			return
+		notification := toast.Notification{
+			AppID:   "Focusd",
+			Title:   title,
+			Message: message,
 		}
-		messagePtr, err := syscall.UTF16PtrFromString(message)
-		if err != nil {
-			return
-		}
-		ret, _, _ := procMessageBoxW.Call(
-			0,
-			uintptr(unsafe.Pointer(messagePtr)),
-			uintptr(unsafe.Pointer(titlePtr)),
-			flags|mbSetForeground,
-		)
-		if callback != nil {
-			callback(ret)
-		}
+		_ = notification.Push()
 	}()
 	return true
 }
 
-func showNotification(title, message string) bool {
-	return show(title, message, mbIconInformation, nil)
-}
-
 func showNotificationWithAction(title, message string, callback func(disable bool)) bool {
-	fullMessage := message + "\n\n[OK] Disable this reminder\n[Cancel] Just close"
-	return show(title, fullMessage, mbOKCancel|mbIconWarning, func(ret uintptr) {
-		if callback != nil {
-			callback(ret == idOK)
+	if time.Since(lastNotification) < 10*time.Second {
+		return false
+	}
+	lastNotification = time.Now()
+
+	go func() {
+		notification := toast.Notification{
+			AppID:   "Focusd",
+			Title:   title,
+			Message: message,
 		}
-	})
+		_ = notification.Push()
+		// ponytail: go-toast is push-and-forget; callback defaults to non-snooze
+		if callback != nil {
+			callback(false)
+		}
+	}()
+	return true
 }
